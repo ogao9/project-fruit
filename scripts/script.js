60c1826750ac32cb0b3f05e05ab71bcd76726edf
unknown <ogao@umich.edu>
2021-05-02 18:51:25 -0400

    updated all 3 pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
-const q1_ans = document.querySelector('#Q1');
+const q1_ans = document.querySelector('.Q1');
+const submit_button = document.querySelector('.submit');
+const display_result = document.querySelector('.result');
+const guesses = document.querySelector('.guesses');
 
-const submit_button = document.querySelector('#submit');
-
-const display_result = document.querySelector('#result');
-
-function checkGuess(){
+function checkGuess(e){
+    e.preventDefault(); //prevents reload of page
     let userGuess = Number(q1_ans.value);
+  
+    guesses.textContent += userGuess + ' ';
+
     if(userGuess === 70){
         display_result.textContent = "CORRECT!";
     }else{
         display_result.textContent = "INCORRECT!";
+        q1_ans.focus();
     }
+    
+    q1_ans.disabled = false;
+    submit_button.disabled = false;
+    q1_ans.value = '';
 }
 
 
